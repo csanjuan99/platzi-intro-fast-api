d22948fc2107f8f33ad181e2cd82fbda4a0acf8b
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2023-03-05 14:23:32 -0500

    02 - Automatic documentation w/swagger

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Documentation
+app.title = "Platzi - Introduction to FastAPI"
+app.version = "0.0.1"
 
-@app.get("/")
+
+@app.get("/", tags=["root"])
 async def root():
     return {"message": "Hello World"}
